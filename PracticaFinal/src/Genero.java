a94537e4bd8720eb06f891814a026b3e96b6d481
Rubén <rgp4lol@gmail.com>
2019-03-15 17:41:47 +0100

    Enum de géneros

---
+++ PracticaFinal/src/Genero.java
@@ -0,0 +1,15 @@
+public enum Genero {
+    DRAMA,
+    CIENCIA_FICCION,
+    TERROR,
+    COMEDIA,
+    ACCION,
+    AVENTURA,
+    INTRIGA,
+    ANIMACION,
+    ROMANCE,
+    BELICO,
+    MUSICAL,
+    HISTORICO,
+    WESTERN
+}
---
+++ PracticaFinal/src/Peliculas.java
@@ -0,0 +1,6 @@
+public class Peliculas {
+    Genero genero;
+    String titulo;
+
+
+}
